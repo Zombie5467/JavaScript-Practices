9028e80e5f258f2b3bba344a2d03fe72c07c5763
Addel <sigmamediacode@gmail.com>
2023-06-05 09:57:36 -0400

    fechas de las agencias

--- Curso JavaScript Moderno/Proyectos/17-PROYECTO-Buscador/js/app.js
+++ Curso JavaScript Moderno/Proyectos/17-PROYECTO-Buscador/js/app.js
@@ -1,17 +1,32 @@
 // Variables
 const resultado = document.querySelector('#resultado');
+const year = document.querySelector('#year');
+const max = new Date().getFullYear();
+const min = max - 10;
 
 // Funciones
 function mostrarAutos() {
   autos.forEach((auto) => {
+    const { marca, modelo, year, puertas, transmision, precio, color } = auto;
     const autoHTML = document.createElement('p');
-    autoHTML.textContent = `${auto.marca}`;
+    autoHTML.textContent = `${marca} ${modelo} - ${year} - ${puertas} Puertas - Transmisión: ${transmision}`;
     // Insertar en HTML
     resultado.appendChild(autoHTML);
   });
 }
 
+// Llena las opciones de años
+function llenarSelect() {
+	for (let i = max; i > min; i--) {
+    const option = document.createElement('option');
+    option.value = i;
+    option.textContent = i;
+    year.appendChild(option);
+  }
+}
+
 // Eventos, esta código estaba puesto ANTES que Funciones
 document.addEventListener('DOMContentLoaded', () => {
-  mostrarAutos();
+  mostrarAutos(); /* Muestra los autos al cargar */
+  llenarSelect(); /* Llena las opciones de años */
 });
